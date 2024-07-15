fbe1ab2b5c6aa973535fca5160ab3be9de68cd74
jhonalen98 <jhon.gonzalez@tecnologicoliceocristiano.edu.ec>
2024-07-15 16:20:54 -0500

    new file:   NumeroMenor.py

---
+++ NumeroMenor.py
@@ -0,0 +1,9 @@
+N1 = int(input("Ingrese primer numero:"))
+N2 = int(input("Ingrese segundo numero:"))
+
+print("Primer numero:",N1)
+print("Segundo numero:",N2)
+if N1<N2:
+    print("El menor es:",N1)
+else:
+    print("El menor es:",N2)

dc2911c87aca8a4f09d3b152a65ab5603d689ad9
jhonalen98 <jhon.gonzalez@tecnologicoliceocristiano.edu.ec>
2024-07-15 16:22:20 -0500

    modified:   NumeroMenor.py

--- NumeroMenor.py
+++ NumeroMenor.py
@@ -1,8 +1,6 @@
 N1 = int(input("Ingrese primer numero:"))
 N2 = int(input("Ingrese segundo numero:"))
 
-print("Primer numero:",N1)
-print("Segundo numero:",N2)
 if N1<N2:
     print("El menor es:",N1)
 else:
